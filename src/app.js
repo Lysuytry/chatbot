c4eed2ff866785dbf22f004701f27860df2ec78e
lysuytry <lysuytry20@gmail.com>
2018-07-15 15:56:47 +0700

    a

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import Bootbot from 'bootbot';
 import 'dotenv/config';
 
 const app = express();
-const bootBot = new Bootbot({
+const bot = new Bootbot({
   accessToken: 'EAAg9uKlXsmABADLO6EBoELi7c4bpCvmYWk8g6eMc49j3ldMyuGzCo2h86Pmgg1KGpnByIClI4nUcbcaegqxdBAb0f0JnuXRZAmaLjwMW1iGFY3yCZByKw8LA6UTbuwAWNcryg1vz3vtaI2rGYTBDDqbFPFQ9zZCMWhr8UKDJwZDZD',
   verifyToken: 'perter-bot',
   appSecret: 'e4dfe78105cb5af810e9484ef259813c'
@@ -17,18 +17,6 @@ app.use(body.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   next();
-});
-
-app.get('/', (req, res) => {
-  return res.status(200).json('hello');
-});
-
-app.get('/webhook/', (req, res) => {
-  return res.status(200).json({'hi' : 'hi'});
-});
-
-app.post('/webhook/', (req, res) => {
-
 });
 
 bot.on('message', (payload, chat) => {
